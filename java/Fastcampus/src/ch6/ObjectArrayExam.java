3068bcbb6420bea12fe6682b9953a1b5013cba48
ygg <rnrn4728@gmail.com>
2023-11-14 18:26:50 +0900

    Array 테스트5

---
+++ java/Fastcampus/src/ch6/ObjectArrayExam.java
@@ -0,0 +1,11 @@
+package ch6;
+
+public class ObjectArrayExam {
+    public static void main(String[] args) {
+        // Q. 영화 3편을 저장할 배열을 생성하고 영화 3편의 데이터를 저장하고 출력하시오.
+        Movie[] m = new Movie[3];
+        m[0] = new Movie();
+        m[1] = new Movie();
+        m[2] = new Movie();
+    }
+}
