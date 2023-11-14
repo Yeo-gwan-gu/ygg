60e1760627affe139c7584267868344a8ddb1d7b
ygg <rnrn4728@gmail.com>
2023-11-14 16:54:49 +0900

    Hello Java 출력

---
+++ java/OOPBasic/src/be7/oop1/FirstHello.java
@@ -0,0 +1,15 @@
+package be7.oop1; // package 선언문
+
+public class FirstHello { // 시작, 메인 클래스
+    public static void main (String[] args) { // 시작점(진입점)
+        // Hello Java 문자열 출력
+        System.out.println("Hello Java"); // 1. 모니터, 콘솔에 출력
+        // 2. 파일에 저장(txt, pdf, excel...)
+        // 3. 브라우저 출력(JavaEE)
+        // 4. 데이터베이스(DB)
+        // 5. 모바일 앱(Android)
+        // 6. GUI(그래픽 유저 인터페이스)
+        // 7. AWS(클라우드)
+        // 8. Network 전송
+    }
+}
