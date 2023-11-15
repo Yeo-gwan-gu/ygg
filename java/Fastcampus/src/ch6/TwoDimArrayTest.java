4f4baf3109c5e9f8bb94c67426864ffe5b809fbc
ygg <rnrn4728@gmail.com>
2023-11-15 15:41:35 +0900

    2차원 배열생성 3(초기화 및 가변길이 배열)

--- java/Fastcampus/src/ch6/TwoDimArrayTest.java
+++ java/Fastcampus/src/ch6/TwoDimArrayTest.java
@@ -17,5 +17,11 @@ public class TwoDimArrayTest {
         System.out.println(a[0][0]+"\t"+a[0][1]+"\t"+a[0][2]+"\t"+a[0][3]);
         System.out.println(a[1][0]+"\t"+a[1][1]+"\t"+a[1][2]+"\t"+a[1][3]);
 
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j]+"\t");
+            }
+            System.out.println();
+        }
     }
 }
