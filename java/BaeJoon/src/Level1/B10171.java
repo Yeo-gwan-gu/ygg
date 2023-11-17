9adbf95f937ea4951141d294a9c48dade2f3c9aa
ygg <rnrn4728@gmail.com>
2023-11-17 16:01:47 +0900

    백준 Lv.1(고양이)

---
+++ java/BaeJoon/src/Level1/B10171.java
@@ -0,0 +1,17 @@
+// 다음과 같이 고양이를 출력하시오.
+//       \    /\
+//        )  ( ')
+//       (  /  )
+//        \(__)|
+
+package Level1;
+
+public class B10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+    }
+}
