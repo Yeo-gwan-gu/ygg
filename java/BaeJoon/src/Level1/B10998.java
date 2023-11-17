fc142a8abad3aa306140b07323175436467cd483
ygg <rnrn4728@gmail.com>
2023-11-17 15:12:14 +0900

    백준 Lv.1(A*B)

---
+++ java/BaeJoon/src/Level1/B10998.java
@@ -0,0 +1,20 @@
+//        문제
+//        두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+//
+//        입력
+//        첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+//
+//        출력
+//        첫째 줄에 A×B를 출력한다.
+package Level1;
+
+import java.util.Scanner;
+
+public class B10998 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        System.out.println(A*B);
+    }
+}
