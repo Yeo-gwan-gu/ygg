9b0f33a281fc992d7d352ad20de8c41412592e3b
ygg <rnrn4728@gmail.com>
2023-11-17 16:04:41 +0900

    백준 Lv.1(개)

---
+++ java/BaeJoon/src/Level1/B10172.java
@@ -0,0 +1,18 @@
+//        |\_/|
+//        |q p|   /}
+//        ( 0 )"""\
+//        |"^"`    |
+//        ||_/=\\__|
+
+package Level1;
+
+public class B10172 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+
+    }
+}
