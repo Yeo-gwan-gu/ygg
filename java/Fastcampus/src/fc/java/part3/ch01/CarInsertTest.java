cf27e92c923bd580cbef89cbcacc3e5df998b344
ygg <rnrn4728@gmail.com>
2023-11-27 16:47:14 +0900

    Car 테스트

---
+++ java/Fastcampus/src/fc/java/part3/ch01/CarInsertTest.java
@@ -0,0 +1,46 @@
+package fc.java.part3.ch01;
+
+import fc.java.model.CarDAO;
+import fc.java.model.CarDTO;
+
+import java.util.Scanner;
+
+public class CarInsertTest {
+    public static void main(String[] args) {
+        // Q. 자동차정보를 키보드로부터 입력을 받아서 DB에 저장을 하시오. (JDBC)
+        Scanner sc = new Scanner(System.in);
+        System.out.print("자동차 일련번호 : ");
+        int carSn = sc.nextInt();
+
+        System.out.print("자동차 이름 : ");
+        String carName = sc.nextLine();
+        sc.nextLine();
+
+        System.out.print("자동차 가격 : ");
+        int carPrice = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("자동차 소유자 : ");
+        String carOwner = sc.nextLine();
+        sc.nextLine();
+
+        System.out.print("자동차 연식 : ");
+        int carYear = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("자동차 타입 : "); // G(휘발유), D(경유)
+        String carType = sc.nextLine();
+
+        CarDTO car = new CarDTO(); // 데이터를 묶어주는 작업
+        car.carSn=carSn;
+        car.carName=carName;
+        car.carPrice=carPrice;
+        car.carOwner=carOwner;
+        car.carYear=carYear;
+        car.carType=carType;
+
+        CarDAO dao = new CarDAO();
+        dao.carInsert(car);
+        dao.carSelect();
+    }
+}
