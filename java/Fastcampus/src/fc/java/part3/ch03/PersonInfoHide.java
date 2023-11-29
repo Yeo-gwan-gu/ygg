3d3a806cbbc4cc288a2807b0089075c8f4dce221
ygg <rnrn4728@gmail.com>
2023-11-29 17:27:05 +0900

    정보은닉

---
+++ java/Fastcampus/src/fc/java/part3/ch03/PersonInfoHide.java
@@ -0,0 +1,15 @@
+package fc.java.part3.ch03;
+
+import fc.java.model.PersonVO;
+
+public class PersonInfoHide {
+    public static void main(String[] args) {
+        // Q. 한사람의 [회원] 정보를 저장할 [객체를 생성] 하시오.
+        PersonVO vo = new PersonVO();
+//        vo.name = "홍길동";
+//        vo.age = 2000; // 정보은닉이 되지 않음
+//        vo.phone = "010-1234-5678";
+//
+//        System.out.println(vo.name+"\t"+vo.age+"\t"+vo.phone);
+    }
+}
