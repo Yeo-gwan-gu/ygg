8bfd3474a7f0a17ad64dc2ea8e27045a0f9a107e
ygg <rnrn4728@gmail.com>
2023-12-03 22:23:54 +0900

    실습과제(MovieProcessor)_메인클래스

--- java/OOPBasic/src/be7/test/MovieProcessor.java
+++ java/OOPBasic/src/be7/test/MovieProcessor.java
@@ -6,12 +6,14 @@ public class MovieProcessor {
 
         Scanner scan = new Scanner(System.in);
         MovieOperations oper= new MovieOperations(100);
+        System.out.println("======== 장르별 영화 검색 프로그램("+"menu"+") ========");
+        System.out.println("1.영화입력(I)" + "\t" + "2.영화출력(P)" + "\t" + "3.영화검색(S)" + "\t" + "4.종료(E)");
+        System.out.println("☑︎ 'E' 또는 '-1'을 입력할 경우 프로그램이 바로 종료됩니다.");
+        System.out.println("=============================================");
+        System.out.print("메뉴입력: ");
+
 
         while (true) {
-            System.out.println("======== 장르별 영화 검색 프로그램(그룹 이름) ========");
-            System.out.println("1.영화입력(I)" + "\t" + "2.영화출력(P)" + "\t" + "3.영화검색(S)" + "\t" + "4.종료(E)");
-            System.out.println("=============================================");
-            System.out.print("메뉴입력: ");
             String menu = scan.nextLine();
 
             if (menu.equalsIgnoreCase("I")) {
@@ -27,6 +29,13 @@ public class MovieProcessor {
                 System.out.println("잘못된 메뉴 입력입니다. 다시 입력해 주세요.");
             }
             System.out.println();
+
+            System.out.println("======== 장르별 영화 검색 프로그램("+menu.toUpperCase()+") ========");
+            System.out.println("1.영화입력(I)" + "\t" + "2.영화출력(P)" + "\t" + "3.영화검색(S)" + "\t" + "4.종료(E)");
+            System.out.println("☑︎ 'E' 또는 '-1'을 입력할 경우 프로그램이 바로 종료됩니다.");
+            System.out.println("=============================================");
+            System.out.print("메뉴입력: ");
+
         } // while_
 
         scan.close();
