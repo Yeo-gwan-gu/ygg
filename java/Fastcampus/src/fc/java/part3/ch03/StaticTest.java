6fac240aca8ae35a26a8b7ac25a45fdb9a669b66
ygg <rnrn4728@gmail.com>
2023-12-04 12:42:07 +0900

    static Test

---
+++ java/Fastcampus/src/fc/java/part3/ch03/StaticTest.java
@@ -0,0 +1,18 @@
+package fc.java.part3.ch03;
+
+public class StaticTest {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        
+        int sum = hap(a,b);
+        System.out.println("sum = " + sum);
+        System.out.println("hap = "+hap(a,b));
+
+    }
+    // Q. 매개변수로 2개의 정수를 입력 받아서 총합을 구하여 리턴하는 메서드를 정의하시오.
+    public static int hap(int a, int b) {
+        int v = a+b;
+        return v;
+    }
+}
