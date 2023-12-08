cdf4a254fc0047483e49457eae2453e489622ebe
ygg <rnrn4728@gmail.com>
2023-12-08 14:28:59 +0900

    추상클래스,메서드 테스트

---
+++ java/Fastcampus/src/fc/java/part4/ch05/AbstractClassTest.java
@@ -0,0 +1,22 @@
+package fc.java.part4.ch05;
+
+
+import fc.java.poly.*;
+
+public class AbstractClassTest {
+    public static void main(String[] args) {
+        // 추상클래스는 단독으로 객체를 생성할 수 없다.
+        // Animal ani = new Animal(); --> cannot be instantiated(객체를 생성할 수 없다.)
+        // 추상클래스는 부모의 역할을 할 수 있다.
+
+        Animal ani = new Dog();
+        ani.eat();
+        ani.move();
+
+        ani = new Cat();
+        ani.eat();
+        ani.move();
+        ((Cat)ani).night();
+
+    }
+}
