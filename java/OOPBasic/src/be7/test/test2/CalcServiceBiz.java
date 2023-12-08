e8108567e02e52bd6c4778e6186158c8df90b773
ygg <rnrn4728@gmail.com>
2023-12-08 17:03:34 +0900

    과제2 연산

---
+++ java/OOPBasic/src/be7/test/test2/CalcServiceBiz.java
@@ -0,0 +1,32 @@
+package be7.test.test2;
+
+public class CalcServiceBiz implements ICalcService {
+
+    @Override
+    public NumberDTO add(NumberDTO number) {
+        double result = number.getNum1() + number.getNum2();
+        number.setResult(result);
+        return number;
+    }
+
+    @Override
+    public NumberDTO sub(NumberDTO number) {
+        double result = number.getNum1() - number.getNum2();
+        number.setResult(result);
+        return number;
+    }
+
+    @Override
+    public NumberDTO mul(NumberDTO number) {
+        double result = number.getNum1() * number.getNum2();
+        number.setResult(result);
+        return number;
+    }
+
+    @Override
+    public NumberDTO div(NumberDTO number) {
+        double result = number.getNum1() / number.getNum2();
+        number.setResult(result);
+        return number;
+    }
+}
