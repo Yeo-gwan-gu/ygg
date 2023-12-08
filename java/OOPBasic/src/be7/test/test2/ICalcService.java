6514408333c0d77995632424de5915331fc4204c
ygg <rnrn4728@gmail.com>
2023-12-08 17:03:08 +0900

    과제2 Interface

---
+++ java/OOPBasic/src/be7/test/test2/ICalcService.java
@@ -0,0 +1,15 @@
+package be7.test.test2;
+
+public interface ICalcService {
+    // public NumberDTO add(numberDTO number);
+    // 기능 추가
+
+    NumberDTO add(NumberDTO number);
+    NumberDTO sub(NumberDTO number);
+    NumberDTO mul(NumberDTO number);
+    NumberDTO div(NumberDTO number);
+
+
+
+
+}
