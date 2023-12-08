88c4898a570b0b945d318ea57c3ef19f074f129e
ygg <rnrn4728@gmail.com>
2023-12-08 14:12:30 +0900

    자식클래스 복사

---
+++ java/Fastcampus/src/fc/java/poly/Dog.java
@@ -0,0 +1,11 @@
+package fc.java.poly;
+
+public class Dog extends Animal {
+//    public void eat() {
+//        System.out.println("개처럼 먹다.");
+//    }
+
+    public void eat() {
+        System.out.println("개처럼 먹다.");
+    }
+}
